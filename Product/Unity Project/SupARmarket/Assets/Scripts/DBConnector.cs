2334c52a677e82a95bb815335aa06353b2ff97ac
S188944 <s188944@uos.ac.uk>
2019-11-24 23:15:50 +0000

    Updated project
    When the image is tracked, it will read data from the database and display it in the debug console.

--- Product/Unity Project/SupARmarket/Assets/Scripts/DBConnector.cs
+++ Product/Unity Project/SupARmarket/Assets/Scripts/DBConnector.cs
@@ -1,31 +1,51 @@
 ﻿using UnityEngine;
+using Vuforia;
 using System.Data.SQLite;
 
-public class DBConnector : MonoBehaviour
+public class DBConnector : MonoBehaviour, ITrackableEventHandler
 {
+    private TrackableBehaviour imageTracker;
+
     void Start()
     {
-        SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\Owner\Documents\Github\SupARmarket-AR-App\Product\Unity Project\SupARmarket\Assets\SQLite\Database.db;Version=3;"); //Locate SQLite Database
-
-        connection.Open(); //Open connection to SQLite database
-
-        Debug.Log("Database connection successful!");
-
-        SQLiteCommand cmnd_read = connection.CreateCommand(); //Create command to read data
-        SQLiteDataReader reader; //Create data reader
-
-        //Query the database
-        string query = "SELECT username FROM User;";
-
-        //Read database command
-        cmnd_read.CommandText = query;
-        reader = cmnd_read.ExecuteReader();
-
-        while (reader.Read())
+        //Create image tracker
+        imageTracker = GetComponent<TrackableBehaviour>();
+        if (imageTracker)
         {
-            Debug.Log("username: " + reader[0].ToString());
+            imageTracker.RegisterTrackableEventHandler(this);
         }
+    }
 
-        connection.Close(); //Close connection to SQLite database
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=" + Application.dataPath + "/SQLite/Database.db;Version=3;"); //Locate SQLite Database
+
+            connection.Open(); //Open connection to SQLite database
+
+            Debug.Log("Database connection successful!");
+
+            SQLiteCommand cmnd_read = connection.CreateCommand(); //Create command to read data
+            SQLiteDataReader reader; //Create data reader
+
+            //Query the database
+            string query = "SELECT username, product_name FROM User NATURAL JOIN UserProduct NATURAL JOIN Product;";
+
+            //Read database command
+            cmnd_read.CommandText = query;
+            reader = cmnd_read.ExecuteReader();
+
+            //Write results of query to debug console
+            while (reader.Read())
+            {
+                Debug.Log(reader[0].ToString() + " has located " + reader[1].ToString() + ".");
+            }
+
+            connection.Close(); //Close connection to SQLite database
+        }
+        else
+        {
+        }
     }
 }
